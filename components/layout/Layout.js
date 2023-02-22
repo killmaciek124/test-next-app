6dcd2be1ea711bd15ef787c69166b77ebbef3ca1
Maciej <maciej@maziuk.com.pl>
2023-02-22 13:11:09 +0100

    h1 add

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,11 @@
-import MainNavigation from './MainNavigation';
-import classes from './Layout.module.css';
+import MainNavigation from "./MainNavigation";
+import classes from "./Layout.module.css";
 
 function Layout(props) {
   return (
     <div>
       <MainNavigation />
+      <h1>HELLO MAN ITS WORKING </h1>
       <main className={classes.main}>{props.children}</main>
     </div>
   );
